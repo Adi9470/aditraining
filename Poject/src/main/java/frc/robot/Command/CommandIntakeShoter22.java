06b12ae65aa8b0a30506a3faef416261742ad1c6
Adi9470 <adidu.hadar@gmail.com>
2025-08-15 13:22:19 +0300

    *

--- Poject/src/main/java/frc/robot/Command/CommandIntakeShoter22.java
+++ Poject/src/main/java/frc/robot/Command/CommandIntakeShoter22.java
@@ -9,9 +9,9 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class TestCommand2 extends SequentialCommandGroup {
-  /** Creates a new TestCommand2. */
-  public TestCommand2() {
+public class CommandIntakeShoter22 extends SequentialCommandGroup {
+  /** Creates a new CommandIntakeShoter22. */
+  public CommandIntakeShoter22() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands();
--- Poject/src/main/java/frc/robot/Command/MovementCommandleft22.java
+++ Poject/src/main/java/frc/robot/Command/MovementCommandleft22.java
@@ -9,9 +9,9 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class TestCommand2 extends SequentialCommandGroup {
-  /** Creates a new TestCommand2. */
-  public TestCommand2() {
+public class MovementCommand extends SequentialCommandGroup {
+  /** Creates a new MovementCommand. */
+  public MovementCommand() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands();
